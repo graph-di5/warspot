3d4f93149d13556dc537803e03f69b56382fed2d
novgosh <novgosh@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-11 08:12:09 +0000

    Консольный WCF чат

---
+++ ConsoleApplication1/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Chat
+{
+    class Server
+    {
+        public static void Main (string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(ChatService));
+            sh.Open();
+
+            Console.WriteLine("Server started");
+            Console.ReadKey();
+        }
+    }
+}
