1b245f96408eef5ad784e514df406582dc0b21a1
vlpolyansky@gmail.com <vlpolyansky@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-13 18:27:08 +0000

    Copied classes 'GameScreen', 'ScreenManager', 'Controller' and 'KeyboardAndMouse' from SkyShoot with some changes.

--- WarSpot.Client.XnaClient/WarSpot.Client.XnaClient/WarSpotGame.cs
+++ WarSpot.Client.XnaClient/WarSpot.Client.XnaClient/WarSpotGame.cs
@@ -14,12 +14,12 @@ namespace WarSpot.Client.XnaClient
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class WarSpotGame : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public WarSpotGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
