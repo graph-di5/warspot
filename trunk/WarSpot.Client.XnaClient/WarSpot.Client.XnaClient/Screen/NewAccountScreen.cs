891d08d3db2e5ec8347c01c16e65487527ab89da
AlexanderKavokin@gmail.com <AlexanderKavokin@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-13 13:10:03 +0000

    screens added

---
+++ trunk/WarSpot.Client.XnaClient/WarSpot.Client.XnaClient/Screen/NewAccountScreen.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarSpot.Client.XnaClient.Screen
+{
+    internal class NewAccountScreen :  GameScreen
+    {
+    }
+}
