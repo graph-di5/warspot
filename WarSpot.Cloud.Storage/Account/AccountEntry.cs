8d8006bb6bf9bb3b016334609fd481d3b5c5723f
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-20 12:06:34 +0000

    Добавлен интерфейс IWarSpotService.cs с методами login(), register(). Добавлен класс WarSpotMainUserService.cs с реализацией вышеуказанных методов. Добавлены классы Account/AccountDataContext.cs, Account/AccountDataSource.cs, Account/AccountEntry.cs , отвечающие за таблицу с именами и паролями.

---
+++ WarSpot.Cloud.Storage/Account/AccountEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace WarSpot.Cloud.Storage.Account
+{
+    public class AccountEntry : Microsoft.WindowsAzure.StorageClient.TableServiceEntity
+
+    {
+        public AccountEntry(string _name, string _pass)
+        {            
+            PartitionKey = DateTime.UtcNow.ToString("MMddyyyy");
+            // Row key allows sorting, so we make sure the rows come back in time order
+            RowKey = string.Format("{0:10}_{1}", DateTime.MaxValue.Ticks - DateTime.Now.Ticks, Guid.NewGuid());
+            this.Name = _name;
+            this.Pass = _pass;
+        }
+
+
+
+        public string Name;
+        public string Pass;
+
+    }
+}
---
+++ WarSpot.Cloud.UserService/WarSpotMainUserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WarSpot.Cloud;
+using WarSpot.Contracts.Service;
+using WarSpot.Cloud.Storage;
+using WarSpot.Cloud.Storage.Account;
+
+
+namespace WarSpot.Cloud.UserService
+{
+    public class WarSpotMainUserService : IWarSpotService
+    {
+
+        public AccountDataSource DataBase;
+
+
+        public bool register(string username, string pass)
+        {
+            return DataBase.AddAccountEntry(new AccountEntry(username, pass));
+        }
+
+        public bool login(string inputUsername, string inputPass)
+        {
+            return DataBase.CheckAccountEntry(inputUsername, inputPass);
+        }
+
+    }
+}
