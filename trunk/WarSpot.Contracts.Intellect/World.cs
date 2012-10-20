0e79cef1aa462d88bf47382d13ab7aa5845e64f4
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-20 10:37:23 +0000

    added actions; added world characteristics

--- trunk/WarSpot.Contracts.Intellect/World.cs
+++ trunk/WarSpot.Contracts.Intellect/World.cs
@@ -1,7 +1,38 @@
 ﻿namespace WarSpot.Contracts.Intellect
 {
+	/// <summary>
+	/// One cell of the whole world
+	/// </summary>
+	public class WorldCell
+	{
+		public int X;
+		public int Y;
+
+		public float Ci;
+		public Being Being;
+	}
+
 	public class World
 	{
+		private WorldCell[,] _content;
+		/// <summary>
+		/// Width
+		/// </summary>
+		private readonly int _w;
 
+		/// <summary>
+		/// Height
+		/// </summary>
+		private readonly int _h;
+
+		/// <summary>
+		/// ctor
+		/// </summary>
+		public World()
+		{
+			_w = 100;
+			_h = 70;
+			_content = new WorldCell[_w, _h];
+		}
 	}
 }
