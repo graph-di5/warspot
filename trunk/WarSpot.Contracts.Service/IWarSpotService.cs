eea09c9eb5bed6c2c98f22d6c862af7a72295946
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-20 12:06:34 +0000

    Добавлен интерфейс IWarSpotService.cs с методами login(), register(). Добавлен класс WarSpotMainUserService.cs с реализацией вышеуказанных методов. Добавлены классы Account/AccountDataContext.cs, Account/AccountDataSource.cs, Account/AccountEntry.cs , отвечающие за таблицу с именами и паролями.

---
+++ trunk/WarSpot.Contracts.Service/IWarSpotService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarSpot.Contracts.Service
+{
+    public interface IWarSpotService
+    {
+
+        bool register(string username, string pass);
+
+        bool login(string inputUsername, string inputPass);
+
+    }
+}
