65698a23aa6b100259bd87487dc8abbe0aacbc5c
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-25 20:15:41 +0000

    updated comments updated paths

--- trunk/WarSpot.Contracts.Intellect/BeingInterface.cs
+++ trunk/WarSpot.Contracts.Intellect/BeingInterface.cs
@@ -2,8 +2,19 @@
 
 namespace WarSpot.Contracts.Intellect
 {
+	/// <summary>
+	/// Iface of all beings in the world
+	/// </summary>
 	public interface IBeingInterface
 	{
+		// todo add arra of worldcells 
+
+		/// <summary>
+		/// Main function of the every being in the world.
+		/// </summary>
+		/// <param name="step">Current time step</param>
+		/// <param name="characteristics">Updated characteristics of the being.</param>
+		/// <returns>Decided action of the being. May be NULL.</returns>
 		GameAction Think(ulong step, BeingCharacteristics characteristics);
 	}
 }
