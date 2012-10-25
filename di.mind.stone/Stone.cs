faf4d2add1f0533a672f12688a85199b1480143b
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-25 19:47:38 +0000

    added sample being

---
+++ di.mind.stone/Stone.cs
@@ -0,0 +1,13 @@
+﻿using WarSpot.Contracts.Intellect;
+using WarSpot.Contracts.Intellect.Actions;
+
+namespace di.mind.stone
+{
+	public class Stone : IBeingInterface
+	{
+		public GameAction Think(ulong step, BeingCharacteristics characteristics)
+		{
+			return new GameAction(characteristics.Id);
+		}
+	}
+}
