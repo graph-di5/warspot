ee04a710237930508dc2bad1cbd2031359b3a4d1
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-27 11:21:21 +0000

    InputValidator updated, added check in register screen

--- WarSpot.Client.XnaClient/Input/InputValidator.cs
+++ WarSpot.Client.XnaClient/Input/InputValidator.cs
@@ -10,12 +10,16 @@ namespace WarSpot.Client.XnaClient.Input
     {
         static public bool checkText(string input)
         {
+            if (input == null)
+                return false;
             string pattern = "^[0-9a-zA-Z_]{6,18}$";
             return Regex.IsMatch(pattern, input);
         }
 
         static public bool checkEmail(string input)
         {
+            if (input == null)
+                return false;
             string pattern = @"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$";
             return Regex.IsMatch(input, pattern);
         }
