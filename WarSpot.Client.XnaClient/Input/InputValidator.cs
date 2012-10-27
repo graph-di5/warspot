0439461229ca6943dada28779cecd4ffd2918793
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-27 11:07:49 +0000

    Added regexps for validating user input

---
+++ WarSpot.Client.XnaClient/Input/InputValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WarSpot.Client.XnaClient.Input
+{
+    static class InputValidator
+    {
+        static public bool checkText(string input)
+        {
+            string pattern = "^[0-9a-zA-Z_]{6,18}$";
+            return Regex.IsMatch(pattern, input);
+        }
+
+        static public bool checkEmail(string input)
+        {
+            string pattern = @"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$";
+            return Regex.IsMatch(input, pattern);
+        }
+    }
+
+}
