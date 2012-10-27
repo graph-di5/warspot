497c57db9ddfc83eb45eea693ff908ae48703a0d
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-27 11:42:37 +0000

    Fixed all bugs with input validation

---
+++ trunk/WarSpot.Client.XnaClient/Utils/InputValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WarSpot.Client.XnaClient.Utils
+{
+    static class InputValidator
+    {
+        static public bool checkText(string input)
+        {
+            if (input == null)
+                return false;
+            string pattern = @"^[0-9a-zA-Z_]{6,18}$";
+            return Regex.IsMatch(input, pattern);
+        }
+
+        static public bool checkEmail(string input)
+        {
+            if (input == null)
+                return false;
+            string pattern = @"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$";
+            return Regex.IsMatch(input, pattern);
+        }
+    }
+
+}
