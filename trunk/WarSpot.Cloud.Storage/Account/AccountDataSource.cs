a45a7e982f2c94eb3fc5fa6b63d723ac6b9a2ee0
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-27 20:31:18 +0000

    small fixes

--- trunk/WarSpot.Cloud.Storage/Account/AccountDataSource.cs
+++ trunk/WarSpot.Cloud.Storage/Account/AccountDataSource.cs
@@ -9,72 +9,74 @@ using WarSpot.Cloud.Storage.Account;
 
 namespace WarSpot.Cloud.Storage.Account
 {
-    public class AccountDataSource
-    {
-        private static CloudStorageAccount storageAccount;
-        private AccountDataContext context;
-        
-        static AccountDataSource()
-        {
-					//CloudStorageAccount.SetConfigurationSettingPublisher(
-					//          (a, b) => b(RoleEnvironment.GetConfigurationSettingValue(a))); storageAccount = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+	public class AccountDataSource
+	{
+		private static CloudStorageAccount storageAccount;
+		private AccountDataContext context;
 
-             CloudTableClient.CreateTablesFromModel(
-             typeof(AccountDataContext),
-             storageAccount.TableEndpoint.AbsoluteUri,
-             storageAccount.Credentials);
-        }
+		static AccountDataSource()
+		{
+			//CloudStorageAccount.SetConfigurationSettingPublisher(
+			//          (a, b) => b(RoleEnvironment.GetConfigurationSettingValue(a)));
 
-        public AccountDataSource()
-        {
-            this.context = new AccountDataContext(storageAccount.TableEndpoint.AbsoluteUri, storageAccount.Credentials);
-            this.context.RetryPolicy = RetryPolicies.Retry(3, TimeSpan.FromSeconds(1));
-        }
+			storageAccount = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+
+			CloudTableClient.CreateTablesFromModel(
+			typeof(AccountDataContext),
+			storageAccount.TableEndpoint.AbsoluteUri,
+			storageAccount.Credentials);
+		}
+
+		public AccountDataSource()
+		{
+			this.context = new AccountDataContext(storageAccount.TableEndpoint.AbsoluteUri, storageAccount.Credentials);
+			this.context.RetryPolicy = RetryPolicies.Retry(3, TimeSpan.FromSeconds(1));
+		}
 
 
-        public bool AddAccountEntry(AccountEntry newItem) // создаем новый аккаунт
-        {
-            var results = (from g in this.context.AccountEntry select g).ToList(); // посмотрим, что у нас уже лежит
-            var entry = results.Where(g => g.Name == newItem.Name).FirstOrDefault<AccountEntry>(); // посмотрим, нет ли аккаунта с таким же
-                                                                                                   // именем
+		public bool AddAccountEntry(AccountEntry newItem) // создаем новый аккаунт
+		{
+			var results = (from g in this.context.AccountEntry select g).ToList(); // посмотрим, что у нас уже лежит
+			var entry = results.Where(g => g.Name == newItem.Name).FirstOrDefault<AccountEntry>(); // посмотрим, нет ли аккаунта с таким же
+			// именем
 
-            if (entry != null) // если есть
-            {
-                return false; // false!!!
-            }
+			if (entry != null) // если есть
+			{
+				return false; // false!!!
+			}
 
-            else // если нет
-            {
-                this.context.AddObject("AccountEntry", newItem); // создаем новую запись в тамблице
-                this.context.SaveChanges();
-                return true; 
-            }
-        }
+			else // если нет
+			{
+				this.context.AddObject("AccountEntry", newItem); // создаем новую запись в тамблице
+				this.context.SaveChanges();
+				return true;
+			}
+		}
 
-        public bool CheckAccountEntry(string username, string pass) // проверяем на совпадение имени\пароля
-        {
-            var results = (from g in this.context.AccountEntry select g).ToList();
-            var entry = results.Where(g => g.Name == username).FirstOrDefault<AccountEntry>();
+		public bool CheckAccountEntry(string username, string pass) // проверяем на совпадение имени\пароля
+		{
+			var results = (from g in this.context.AccountEntry select g).ToList();
+			var entry = results.Where(g => g.Name == username).FirstOrDefault<AccountEntry>();
 
-            if (entry != null) // нашли аккаунт с нужным именем
-            {
-                if (entry.Pass == pass) // проверяем правильность пароля
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else // не нашли аккаунт с нужным именем
-            {
-                return false;
-            }
-            
-        }
+			if (entry != null) // нашли аккаунт с нужным именем
+			{
+				if (entry.Pass == pass) // проверяем правильность пароля
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else // не нашли аккаунт с нужным именем
+			{
+				return false;
+			}
 
-        
+		}
 
-    }
+
+
+	}
 }
--- trunk/WarSpot.Cloud.UserService/WarSpotMainUserService.cs
+++ trunk/WarSpot.Cloud.UserService/WarSpotMainUserService.cs
@@ -25,25 +25,25 @@ namespace WarSpot.Cloud.UserService
 				new ErrorCode(ErrorType.WrongLoginOrPassword);
 		}
 
-        public ErrorCode UploadIntellect(byte[] data, string name)
-        {
-            // Проверка и что-то типа return isCorrect(intellect) ? new ErrorCode(ErrorType.ok) :
-            // new ErrorCode(ErrorType.ForbiddenUsages)
-            return new ErrorCode(ErrorType.Ok);
-        }
-        public string[] GetListOfIntellects()
-        {
-            // TODO: получать список всех интеллектов пользователя и возвращать их массивом
+		public ErrorCode UploadIntellect(byte[] data, string name)
+		{
+			// Проверка и что-то типа return isCorrect(intellect) ? new ErrorCode(ErrorType.ok) :
+			// new ErrorCode(ErrorType.ForbiddenUsages)
+			return new ErrorCode(ErrorType.Ok);
+		}
+		public string[] GetListOfIntellects()
+		{
+			// TODO: получать список всех интеллектов пользователя и возвращать их массивом
 
-            // Заглушка
-            string[] intellects = new string[20];
-            return intellects;
-        }
+			// Заглушка
+			string[] intellects = new string[20];
+			return intellects;
+		}
 
 		public ErrorCode DeleteIntellect(string name)
 		{
 			// TODO: корректная работа с удалением
 			return new ErrorCode(ErrorType.Ok);
 		}
-    }
+	}
 }
