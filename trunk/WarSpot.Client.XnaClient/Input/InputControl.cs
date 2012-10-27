ae6470fc54c01870e22518f1eb3bab244a40ff08
vlpolyansky@gmail.com <vlpolyansky@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-27 16:31:19 +0000

    Less bugs in InputControl

--- trunk/WarSpot.Client.XnaClient/Input/InputControl.cs
+++ trunk/WarSpot.Client.XnaClient/Input/InputControl.cs
@@ -1,4 +1,5 @@
-﻿namespace WarSpot.Client.XnaClient.Input
+﻿using System;
+namespace WarSpot.Client.XnaClient.Input
 {
     internal class InputControl : Nuclex.UserInterface.Controls.Desktop.InputControl
     {
@@ -13,20 +14,64 @@
             return new string('*', text.Length);
         }
 
+		protected override bool OnButtonPressed(Microsoft.Xna.Framework.Input.Buttons button)
+		{
+			return false;
+		}
+
+		protected override bool OnKeyPressed(Microsoft.Xna.Framework.Input.Keys keyCode)
+		{
+			switch (keyCode)
+			{
+				case Microsoft.Xna.Framework.Input.Keys.Left:
+					CaretPosition = CaretPosition > 0 ? CaretPosition - 1 : 0;
+					return true;
+				case Microsoft.Xna.Framework.Input.Keys.Right:
+					CaretPosition = CaretPosition < RealText.Length ? CaretPosition + 1 : CaretPosition;
+					return true;
+				case Microsoft.Xna.Framework.Input.Keys.Delete:
+					if (CaretPosition < RealText.Length)
+					{
+						RealText = RealText.Substring(0, CaretPosition) + RealText.Substring(CaretPosition + 1);
+						_passwordLength--;
+						Text = IsHidden ? new string('*', _passwordLength) : RealText;
+					}
+					return true;
+				default:
+					return false;
+			}
+		}
+
+		protected override void OnButtonReleased(Microsoft.Xna.Framework.Input.Buttons button)
+		{
+		}
+
+		protected override void OnKeyReleased(Microsoft.Xna.Framework.Input.Keys keyCode)
+		{
+		}
+
         protected override void OnCharacterEntered(char character)
         {
-            if (character == '\b' && _passwordLength != 0)
+            if (character == '\b' && CaretPosition > 0)
             {
-                RealText = RealText.Substring(0, _passwordLength - 1);
-                _passwordLength--;
+				int cp = CaretPosition;
+				RealText = RealText.Substring(0, CaretPosition - 1) + RealText.Substring(CaretPosition);
+				//RealText = RealText.Substring(0, _passwordLength - 1);
+				_passwordLength--;
+				Text = IsHidden ? new string('*', _passwordLength) : RealText;
+				CaretPosition = cp - 1;
             }
-            else if (character != '\b' && character != '\t' && character != '\n')
-            {
-                _passwordLength++;
-                RealText += character;
-                Text += IsHidden ? '*' : character;
-                CaretPosition++;
-            }
+			else if (character != '\b' && character != '\t' && character != '\n')
+			{
+				if (RealText == null)
+				{
+					RealText = "";
+				}
+				RealText = RealText.Substring(0, CaretPosition) + character + RealText.Substring(CaretPosition);
+				_passwordLength++;
+				Text = IsHidden ? new string('*', _passwordLength) : RealText;
+				CaretPosition++;
+			}
         }
     }
 }
