23161d7c3fc210b47b7227e1828ddb0a38144731
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-27 13:14:07 +0000

    minor changes in client's architecture

--- WarSpot.Client.XnaClient/AIManager/Intellect.cs
+++ WarSpot.Client.XnaClient/AIManager/Intellect.cs
@@ -3,6 +3,13 @@
     class Intellect
     {
         public string Name { get; set; }
+        public byte[] byteDll;
         public int SizeInBits { get; set; }
+
+        public Intellect(string path, string name)
+        {
+            this.byteDll = System.IO.File.ReadAllBytes(path);
+            this.Name = name;
+        }
     }
 }
