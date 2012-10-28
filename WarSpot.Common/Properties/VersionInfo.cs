879d5851df5abd6357df20054fa2a1cb5401ec72
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-28 21:12:11 +0000

    added autoversion

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -1,18 +1,16 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyCompany("WarSpot team")]
 [assembly: AssemblyCopyright("Copyright © WarSpot 2012")]
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+//        Major Version
+//        Minor Version 
+// (Auto) Revision
+// (Auto) Date stamp
+// 
+// Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
+[assembly: AssemblyVersion("1.0.155.7")]
+[assembly: AssemblyFileVersion("1.0.155.7")]
