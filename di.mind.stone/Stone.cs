17a396907befd93b23b3100d1bc6db960257978d
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-10-29 14:43:04 +0000

    updated stone dll

--- di.mind.stone/Stone.cs
+++ di.mind.stone/Stone.cs
@@ -1,13 +1,19 @@
-﻿using WarSpot.Contracts.Intellect;
+﻿using System;
+using WarSpot.Contracts.Intellect;
 using WarSpot.Contracts.Intellect.Actions;
 
 namespace di.mind.stone
 {
 	public class Stone : IBeingInterface
 	{
-		public GameAction Think(ulong step, BeingCharacteristics characteristics)
+		public BeingCharacteristics Construct(ulong step, float ci, IWorldCell[,] area)
 		{
-			return new GameAction(characteristics.Id);
+			return new BeingCharacteristics(Guid.NewGuid(), 100500, 0, 1);
+		}
+
+		public GameAction Think(ulong step, BeingCharacteristics characteristics, IWorldCell[,] area)
+		{
+			return new GameActionEat(characteristics.Id);
 		}
 	}
 }
