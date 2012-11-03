e08139ec7d12917eed232d30f424aa91c9dc7d34
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-03 11:19:14 +0000

    1.Добавлено действие "умирание существ" (GameActionDie). Может быть вызвано и игроком. 2.Добавлена проверка мёртвых существ и исключения их из списка, с генерацией события в историю.

---
+++ WarSpot.Contracts.Intellect/Actions/GameActionDie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WarSpot.Contracts.Intellect.Actions
+{
+	public class GameActionDie : GameAction
+	{
+		public GameActionDie(Guid senderId) : base(senderId)
+		{
+		}
+
+		public override float Cost()
+		{
+			return 0;
+		}
+
+		public override void Execute()
+		{
+			//Видимо, здесь будет выброс энергии из трупа.
+			throw new NotImplementedException();
+		}
+	}
+}
