dd607350860d008436c078a39ca46e257d9584f5
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-10 10:24:09 +0000

    Добавлен скрипт БД - DBModel.edmx.sql
    Добавлена работа с SQL.
    
    Добавлен класс Storage.cs, ответственный за BLOB и удалёны папки Account и Intellect.
    
    При построении вылазит ошибка :(

---
+++ WarSpot.Cloud.Storage/Storage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+
+namespace WarSpot.Cloud.Storage
+{
+    public class Storage
+    {
+        private static bool storageInitialized = false;
+        private static object gate = new object();
+
+        private static CloudBlobClient blobStorage;
+
+        private void InitializeStorage()
+        {
+            if (storageInitialized)
+            {
+                return;
+            }
+
+            lock (gate)
+            {
+                if (storageInitialized)
+                {
+                    return;
+                }
+
+                try
+                {
+                    // read account configuration settings
+                    var storageAccount = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+
+                    // create blob container for images
+                    blobStorage = storageAccount.CreateCloudBlobClient();
+                    CloudBlobContainer container = blobStorage.GetContainerReference("intellects");
+                    container.CreateIfNotExist();
+
+                    // configure container for public access
+                    var permissions = container.GetPermissions();
+                    permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                    container.SetPermissions(permissions);
+                }
+                catch (WebException)
+                {
+                    throw new WebException("Storage services initialization failure. "
+                       + "Check your storage account configuration settings. If running locally, "
+                       + "ensure that the Development Storage service is running.");
+                }
+
+                storageInitialized = true;
+            }
+        }
+
+        public void Upload(Guid Account_ID, string name, byte[] data)
+        {
+            this.InitializeStorage();
+
+            string uniqueBlobName = string.Format("intellects/{0}/{1}", Account_ID.ToString(), name);
+            CloudBlockBlob blob = blobStorage.GetBlockBlobReference(uniqueBlobName);
+            blob.UploadByteArray(data);
+
+        }
+
+    }
+}
