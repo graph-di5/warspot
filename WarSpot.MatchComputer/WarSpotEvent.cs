70d8c9d2ad827b3f1f8b6028ba37e1289445fe7b
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-10 12:04:10 +0000

    added events

---
+++ WarSpot.MatchComputer/WarSpotEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WarSpot.MatchComputer
+{
+	public abstract class WarSpotEvent
+	{
+
+	}
+
+	public abstract class GameEvent : WarSpotEvent
+	{
+		protected GameEvent(Guid creator)
+		{
+			SubjectId = creator;
+		}
+
+		public Guid SubjectId { get; private set; }
+	}
+
+	// 		public Guid TargetID { private set; get; }
+	public class GameEventHealthChange
+	{
+		public GameEventHealthChange(float newHealth)
+		{
+			Health = newHealth;
+		}
+
+		/// <summary>
+		/// new health
+		/// </summary>
+		public float Health { get; private set; }
+	}
+
+	public abstract class SystemEvent : WarSpotEvent
+	{
+	}
+
+	public class SystemEventCommandDead
+	{
+		public SystemEventCommandDead(int teamId)
+		{
+			TeamId = teamId;
+		}
+
+		public int TeamId { get; private set; }
+	}
+}
