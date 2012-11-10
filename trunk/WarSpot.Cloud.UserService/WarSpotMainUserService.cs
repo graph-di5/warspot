c72cb3f99205f187078f0914e98627eea6ae3359
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-10 10:24:09 +0000

    Добавлен скрипт БД - DBModel.edmx.sql
    Добавлена работа с SQL.
    
    Добавлен класс Storage.cs, ответственный за BLOB и удалёны папки Account и Intellect.
    
    При построении вылазит ошибка :(

--- trunk/WarSpot.Cloud.UserService/WarSpotMainUserService.cs
+++ trunk/WarSpot.Cloud.UserService/WarSpotMainUserService.cs
@@ -1,70 +1,116 @@
-﻿using WarSpot.Cloud.Storage.Account;
+﻿
 using WarSpot.Contracts.Service;
+using WarSpot.Cloud.Storage;
+using System.Data;
 
 namespace WarSpot.Cloud.UserService
 {
-	public class WarSpotMainUserService : IWarSpotService
-	{
-		public AccountDataSource DataBase;
+    public class WarSpotMainUserService : IWarSpotService
+    {
+        public DBContext db;      
 
-		public bool loggedIn;
+        public bool loggedIn;
 
+        public System.Guid user_ID;
 
-		public WarSpotMainUserService()
-		{
-			DataBase = new AccountDataSource();
-			loggedIn = false;
-		}
+        public Storage.Storage blob;
 
-		public ErrorCode Register(string username, string pass)
-		{
-			return DataBase.AddAccountEntry(new AccountEntry(username, pass)) ?
-				new ErrorCode(ErrorType.Ok) :
-				new ErrorCode(ErrorType.UnknownException); ;
-		}
+        public WarSpotMainUserService()
+        {
+            db = new DBContext();
+            loggedIn = false;            
+        }
 
-		public ErrorCode Login(string inputUsername, string inputPass)
-		{
-			if (DataBase.CheckAccountEntry(inputUsername, inputPass))
-			{
-				loggedIn = true;
-				return new ErrorCode(ErrorType.Ok);
-			}
-			else
-				return new ErrorCode(ErrorType.WrongLoginOrPassword);
-		}
+        public ErrorCode Register(string username, string pass)
+        {
+            var test = (from b in db.Account
+                        where b.Account_Name = username
+                        select b).ToList<Account>();
+            if (test != null)
+                return new ErrorCode(ErrorType.WrongLoginOrPassword);
+            else
+            {
+                db.AddToAccount(Account.CreateAccount(new System.Guid(), username, pass));
+                return new ErrorCode(ErrorType.Ok);
+            }
+        }
 
-		public ErrorCode UploadIntellect(byte[] intellect, string name)
-		{
-			throw new System.NotImplementedException();
-		}
+        public ErrorCode Login(string username, string pass)
+        {
+            var test = (from b in db.Account
+                        where b.Account_Name = username
+                        select b).ToList<Account>();
 
-		public ErrorCode UploadIntellect(byte[] data)
-		{
-			// Проверка и что-то типа return isCorrect(intellect) ? new ErrorCode(ErrorType.ok) :
-			// new ErrorCode(ErrorType.ForbiddenUsages)
+            var res = test.Where(b => b.Account_Password == pass).FirstOrDefault<Account>();
 
-			if (loggedIn)
-			{
-				DataBase.UploadDLL(data);
-				return new ErrorCode(ErrorType.Ok);
-			}
-			else
-				return new ErrorCode(ErrorType.ForbiddenUsages);
-		}
-		public string[] GetListOfIntellects()
-		{
-			// TODO: получать список всех интеллектов пользователя и возвращать их массивом
+            if (res == null)
+            {
+                return new ErrorCode(ErrorType.WrongLoginOrPassword);
+            }
+            else
+            {
+                loggedIn = true;
+                user_ID = (from b in db.Account
+                           where b.Account_Name = username
+                           select b).Account_ID;
 
-			// Заглушка
-			string[] intellects = new string[20];
-			return intellects;
-		}
+                return new ErrorCode(ErrorType.Ok);
+            }
 
-		public ErrorCode DeleteIntellect(string name)
-		{
-			// TODO: корректная работа с удалением
-			return new ErrorCode(ErrorType.Ok);
-		}
-	}
+        }
+
+        public ErrorCode UploadIntellect(byte[] intellect, string name)
+        {
+            if (loggedIn)
+            {
+                blob.Upload(user_ID, name, intellect);
+                return new ErrorCode(ErrorType.Ok);
+            }
+            else
+                return new ErrorCode(ErrorType.WrongLoginOrPassword);
+        }
+
+        
+        public string[] GetListOfIntellects()
+        {
+            // TODO: получать список всех интеллектов пользователя и возвращать их массивом
+
+            // Заглушка
+            string[] result = new string[20];
+            int i = 0;
+
+            var test = (from b in db.Intellect
+                        where b.AccountAccount_ID = user_ID
+                        select b).ToList<Intellect>;
+
+            foreach (Intellect j in test)
+            {
+                result[i] = j.Intellect_Name;
+                i++;
+            }
+
+            return result;
+        }
+
+        public ErrorCode DeleteIntellect(string name)
+        {
+            // TODO: корректная работа с удалением
+
+            var test = (from b in db.Intellect
+                        where b.AccountAccount_ID = user_ID
+                        select b).ToList<Intellect>;
+            var result = test.Where(b => b.Intellect_Name == name);
+
+            if (result)
+            {
+               // db.DeleteFromIntellect(result);
+                return new ErrorCode(ErrorType.Ok);
+            }
+            else
+            {
+                return new ErrorCode(ErrorType.BadFileType);
+            }
+
+        }
+    }
 }
