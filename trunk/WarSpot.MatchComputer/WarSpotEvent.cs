a9e05a549cecc1497ce8a2313986c32f2aca10cd
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-16 23:09:03 +0000

    1. Сериализация истории событий. Складывается в буфер одним куском. ToDo: Договориться, каким образом выгружаем это хэндлеру, реализовать. Дописать закрытие молотилки.

--- trunk/WarSpot.MatchComputer/WarSpotEvent.cs
+++ trunk/WarSpot.MatchComputer/WarSpotEvent.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using WarSpot.XNA.Framework;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace WarSpot.MatchComputer
 {
+	public enum EventTypes { GameEventHealthChange, GameEventCiChange, GameEventMove, GameEventDeath, GameEventBirth,
+		GameEventWorldCiChanged, SystemEventCommandDead, SystemEventCommandWin, SystemEventMatchEnd};
+
+	[Serializable]
 	public abstract class WarSpotEvent
 	{
-
+		/// <summary>
+		/// Type of the event.
+		/// </summary>
+		public WarSpot.MatchComputer.EventTypes EventType { set; get; }
 	}
 
+	[Serializable]
 	public abstract class GameEvent : WarSpotEvent
 	{
 		protected GameEvent(Guid creator)
@@ -18,11 +27,13 @@ namespace WarSpot.MatchComputer
 		public Guid SubjectId { get; private set; }
 	}
 
+	[Serializable]
 	public class GameEventHealthChange : GameEvent
 	{
 		public GameEventHealthChange(Guid subjectId, float newHealth) :
 			base(subjectId)
 		{
+			EventType = EventTypes.GameEventHealthChange;
 			Health = newHealth;
 		}
 
@@ -32,11 +43,13 @@ namespace WarSpot.MatchComputer
 		public float Health { get; private set; }
 	}
 
+	[Serializable]
 	public class GameEventCiChange : GameEvent
 	{
 		public GameEventCiChange(Guid subjectId, float newCi) :
 			base(subjectId)
 		{
+			EventType = EventTypes.GameEventCiChange;
 			Ci = newCi;
 		}
 
@@ -46,11 +59,13 @@ namespace WarSpot.MatchComputer
 		public float Ci { get; private set; }
 	}
 
+	[Serializable]
 	public class GameEventMove : GameEvent
 	{
 		public GameEventMove(Guid subjectId, int shiftX, int shiftY):
 			base(subjectId)
 		{
+			EventType = EventTypes.GameEventMove;
 			ShiftX = shiftX;
 			ShiftY = shiftY;
 		}
@@ -60,24 +75,30 @@ namespace WarSpot.MatchComputer
 		public int ShiftY { get; private set; }
 	}
 
+	[Serializable]
 	public class GameEventDeath : GameEvent
 	{
 		public GameEventDeath(Guid creator) : base(creator)
 		{
+			EventType = EventTypes.GameEventDeath;
 		}
 	}
 
+	[Serializable]
 	public class GameEventBirth : GameEvent
 	{
 		public GameEventBirth(Guid creator) : base(creator)
 		{
+			EventType = EventTypes.GameEventBirth;
 		}
 	}
 
+	[Serializable]
 	public class GameEventWorldCiChanged : WarSpotEvent
 	{
 		public GameEventWorldCiChanged(int x, int y, float ci)
 		{
+			EventType = EventTypes.GameEventWorldCiChanged;
 			X = x;
 			Y = y;
 			Ci = ci;
@@ -93,31 +114,41 @@ namespace WarSpot.MatchComputer
 		public float Ci { get; private set; }
 	}
 
+	[Serializable]
 	public abstract class SystemEvent : WarSpotEvent
 	{
 	}
 
+	[Serializable]
 	public class SystemEventCommandDead : SystemEvent
 	{
 		public SystemEventCommandDead(int teamId)
 		{
+			EventType = EventTypes.SystemEventCommandDead;
 			TeamId = teamId;
 		}
 
 		public int TeamId { get; private set; }
 	}
 
+	[Serializable]
 	public class SystemEventCommandWin : SystemEvent
 	{
 		public SystemEventCommandWin(int teamId)
 		{
+			EventType = EventTypes.SystemEventCommandWin;
 			TeamId = teamId;
 		}
 
 		public int TeamId { get; private set; }
 	}
 
+	[Serializable]
 	public class SystemEventMatchEnd : SystemEvent
 	{
+		public SystemEventMatchEnd()
+		{
+			EventType = EventTypes.SystemEventMatchEnd;
+		}
 	}
 }
