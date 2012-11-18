79cfc33745c69cbd37b11ecc399f53cd9bdd013b
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-18 15:17:25 +0000

    Addition for last commit

---
+++ trunk/WarSpot.Client.XnaClient/Screen/WatchReplayScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nuclex.UserInterface;
+using Nuclex.UserInterface.Controls;
+using Nuclex.UserInterface.Controls.Desktop;
+using InputControl = WarSpot.Client.XnaClient.Input.InputControl;
+using WarSpot.Client.XnaClient.Network;
+using WarSpot.Contracts.Service;
+
+namespace WarSpot.Client.XnaClient.Screen
+{
+	internal class WatchReplayScreen : GameScreen
+	{
+		private static Texture2D _texture;
+
+		public WatchReplayScreen()
+		{
+			CreateControls();
+			InitializeControls();
+		}
+
+		public override void LoadContent()
+		{
+			_texture = ContentManager.Load<Texture2D>("Textures/Screens/screen_02");
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			SpriteBatch.Begin();
+			SpriteBatch.Draw(_texture, WarSpotGame.Instance.GetScreenBounds(), Color.White);
+			SpriteBatch.End();
+		}
+
+		private void CreateControls()
+		{
+			
+		}
+
+		private void InitializeControls()
+		{
+			
+		}
+	}
+}
