0f533c8553c5ee2ade6e424536824c76c267c2e9
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-18 16:33:12 +0000

    1.Добавил обработку действия GameActionMakeOffspring в молотилке. 2.Дописал добавление событий рождения в начале игры и при создании потомка.

--- WarSpot.Contracts.Intellect/Actions/GameActionMakeOffspring.cs
+++ WarSpot.Contracts.Intellect/Actions/GameActionMakeOffspring.cs
@@ -7,12 +7,24 @@ namespace WarSpot.Contracts.Intellect.Actions
 {
 	public class GameActionMakeOffspring: GameAction
 	{
+
+		public int X;
+		public int Y;
+		public float MaxHealth;
+		public float MaxStep;
+		public int MaxSeeDistance;
+
 		/// <summary>
 		/// Make an offspring.
 		/// </summary>
-		public GameActionMakeOffspring(Guid senderId) : base(senderId)
+		public GameActionMakeOffspring(Guid senderId, int x, int y, float maxHealth, float maxStep, int maxSeeDistance ) : base(senderId)
 		{
 			ActionType = ActionTypes.GameActionMakeOffspring;
+			X = x;
+			Y = y;
+			MaxHealth = maxHealth;
+			MaxStep = maxStep;
+			MaxSeeDistance = maxSeeDistance;
 		}
 	}
 }
