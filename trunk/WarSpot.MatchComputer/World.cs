c8da733dbe91929a5c505c86ce438bebf862fb56
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-19 13:20:44 +0000

    1.Перегрузил индексатор World[x,y]. При задании координат, выходящих за границы мира, работает для клеток за границей (зацикленность мира в обе стороны).

--- trunk/WarSpot.MatchComputer/World.cs
+++ trunk/WarSpot.MatchComputer/World.cs
@@ -5,7 +5,52 @@ namespace WarSpot.MatchComputer
 	/// </summary>
 	class World
 	{
+		public WorldCell this[int x, int y]//Перегрузка индексатора
+		{
+			set
+			{
+				int _x, _y;
+
+				if (x > this.Width)
+					_x = x % this.Width;//Вычитаем лишнее				
+				else if (x < 0)
+					_x = this.Width - (x % this.Width);
+				else
+					_x = x;
+
+				if (y > this.Height)
+					_y = y % this.Height;
+				else if (y < 0)
+					_y = this.Height - (y % this.Height);
+				else
+					_y = y;
+				
+				this.Map[_x, _y] = value;//Обращаемся к нужной точке.
+			}
+			get
+			{
+				int _x, _y;
+
+				if (x > this.Width)
+					_x = x % this.Width;		
+				else if (x < 0)
+					_x = this.Width - (x % this.Width);
+				else
+					_x = x;
+
+				if (y > this.Height)
+					_y = y % this.Height;
+				else if (y < 0)
+					_y = this.Height - (y % this.Height);
+				else
+					_y = y;
+
+				return this.Map[_x, _y];
+			}
+		}
+
 		private WorldCell[,] _content;
+
 		public WorldCell[,] Map
 		{
 			get
