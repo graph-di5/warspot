b8f2bd5d0d36977cd72133abcd04ff804661a4ea
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-19 22:15:13 +0000

    1.Дописал оффлайновый запускатор матча. Правда он не работает.

--- trunk/OfflineMatcher/OfflineMatcher.cs
+++ trunk/OfflineMatcher/OfflineMatcher.cs
@@ -16,20 +16,42 @@ namespace OfflineMatcher
 			string _directory;
 			List<TeamIntellectList> _listIntellect = new List<TeamIntellectList>();
 			TeamIntellectList _firstTeam = new TeamIntellectList();
+			TeamIntellectList _secondTeam = new TeamIntellectList();
 			_firstTeam.Number = 1;
-					
-			Console.WriteLine("Enter the directory with you AI's: ");
+			_secondTeam.Number = 2;
+
+			Console.WriteLine("Enter the directory with first team's AIs: ");
 			_directory = Console.ReadLine();
-			DirectoryInfo dir = new DirectoryInfo(_directory);
-			var _items = dir.GetFiles();
-			Console.WriteLine("1");
-			foreach (var item in _items)
+			DirectoryInfo dir1 = new DirectoryInfo(_directory);
+			var _items1 = dir1.GetFiles();
+			Console.WriteLine("DLL for team 1: ");
+			foreach (var item in _items1)
 			{
 				Console.WriteLine(item.FullName);
 				_firstTeam.Members.Add(ParseIntellect(item.FullName));
 			}
 
-			//Console.WriteLine(_firstTeam.Members.Count());
+			Console.WriteLine("Enter the directory with second team's AIs: ");
+			_directory = Console.ReadLine();
+			DirectoryInfo dir2 = new DirectoryInfo(_directory);
+			var _items2 = dir2.GetFiles();
+			Console.WriteLine("DLL for team 2: ");
+			foreach (var item in _items2)
+			{
+				Console.WriteLine(item.FullName);
+				_secondTeam.Members.Add(ParseIntellect(item.FullName));
+			}
+
+			Console.WriteLine("Enter the directory for serialized match history: ");
+			_directory = Console.ReadLine();
+
+			Console.WriteLine("First team: {0}. Second team: {1}.",_firstTeam.Members.Count(), _secondTeam.Members.Count());
+			Console.WriteLine("Computing...");
+			FileStream fs = new FileStream(_directory + "file.s", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+			var _matcher = new ComputerMatcher(_listIntellect, fs);
+			while (_matcher.Update() != 0) ;//Гоняем update пока кто-то не выиграет.
+			fs.Close();
+			Console.WriteLine("All done. Take your match history in: {0}", _directory);
 			Console.ReadLine();
 			}
 
