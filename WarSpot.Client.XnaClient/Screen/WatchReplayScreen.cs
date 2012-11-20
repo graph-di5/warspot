bf837aa55aade2383705de402c7b9f1e244cbdc6
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-20 20:16:51 +0000

    Replay screen for client

--- WarSpot.Client.XnaClient/Screen/WatchReplayScreen.cs
+++ WarSpot.Client.XnaClient/Screen/WatchReplayScreen.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-
+using Nuclex.UserInterface;
+using Nuclex.UserInterface.Controls;
+using Nuclex.UserInterface.Controls.Desktop;
 namespace WarSpot.Client.XnaClient.Screen
 {
+	// TODO: filling listbox and deleting replays
 	internal class WatchReplayScreen : GameScreen
 	{
 		private static Texture2D _texture;
+
+		private ButtonControl _watchButton;
+		private ButtonControl _deleteButton;
+
+		private LabelControl _replayLabel;
+
+		private Nuclex.UserInterface.Controls.Desktop.ListControl _replaysBox;
 
 		public WatchReplayScreen()
 		{
@@ -27,12 +38,70 @@ namespace WarSpot.Client.XnaClient.Screen
 
 		private void CreateControls()
 		{
+			_replaysBox = new Nuclex.UserInterface.Controls.Desktop.ListControl
+			{
+				SelectionMode = ListSelectionMode.Single,
+				Bounds =
+					new UniRectangle(
+						new UniScalar(0f, 0),
+						new UniScalar(0f, 20),
+						new UniScalar(0f, 300),
+						new UniScalar(0f, 300))
+			};
 
+			_replaysBox.Slider.Bounds.Location.X.Offset -= 1.0f;
+			_replaysBox.Slider.Bounds.Location.Y.Offset += 1.0f;
+			_replaysBox.Slider.Bounds.Size.Y.Offset -= 2.0f;
+			_replaysBox.SelectionMode = Nuclex.UserInterface.Controls.Desktop.ListSelectionMode.Single;
+			_replaysBox.SelectedItems.Add(0);
+
+			_replayLabel = new LabelControl("Select replay:")
+			{
+				Bounds = new UniRectangle(new UniScalar(0f, 0), new UniScalar(0f, 0), 0, 0)
+			};
+
+			_watchButton = new ButtonControl
+			{
+				Text = "Watch",
+				Bounds =
+						new UniRectangle(
+								new UniScalar(0f, 300 + 10),
+								new UniScalar(0f, 20),
+								new UniScalar(0f, 100),
+								new UniScalar(0f, 30))
+			};
+
+			_deleteButton = new ButtonControl
+			{
+				Text = "Delete",
+				Bounds =
+						new UniRectangle(
+								new UniScalar(0f, 300 + 10),
+								new UniScalar(0.1f, 0),
+								new UniScalar(0f, 100),
+								new UniScalar(0f, 30))
+			};
 		}
 
 		private void InitializeControls()
 		{
+			Desktop.Children.Add(_deleteButton);
+			Desktop.Children.Add(_watchButton);
 
+			Desktop.Children.Add(_replayLabel);
+
+			Desktop.Children.Add(_replaysBox);
+
+			ScreenManager.Instance.Controller.AddListener(_watchButton, watchButtonPressed);
+			ScreenManager.Instance.Controller.AddListener(_deleteButton, deleteButtonPressed);
+		}
+
+		private void watchButtonPressed(object sender, EventArgs e)
+		{
+		}
+
+		private void deleteButtonPressed(object sender, EventArgs e)
+		{
 		}
 	}
 }
