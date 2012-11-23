b88ef4bafbeca4619751e149260efc5b84946071
qwwdfsad@gmail.com <qwwdfsad@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-23 14:54:09 +0000

    Added replay-deserializator

---
+++ WarSpot.Client.XnaClient/OfflineMatcher/Deserializator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using Intellect = WarSpot.Contracts.Intellect;
+
+namespace WarSpot.Client.XnaClient.OfflineMatcher
+{
+	/// <summary>
+	/// Returns deserializated list of GameActions from replay
+	/// </summary>
+	static class Deserializator
+	{
+		static List<Intellect.Actions.GameAction> Deserialize(string path)
+		{
+			List<Intellect.Actions.GameAction> deserializedActions = new List<Intellect.Actions.GameAction>();
+			FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+			BinaryFormatter bf = new BinaryFormatter();
+			deserializedActions = (List<Intellect.Actions.GameAction>)bf.Deserialize(fs);
+			fs.Close();
+			return deserializedActions;
+		}
+	}
+}
