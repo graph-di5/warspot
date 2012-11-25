9b0055cfa8d644dcb3f53429e82de747fd5e3542
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-25 20:19:38 +0000

    fixed Team

--- di.mind.stone/Stone.cs
+++ di.mind.stone/Stone.cs
@@ -6,9 +6,9 @@ namespace di.mind.stone
 {
 	public class Stone : IBeingInterface
 	{
-		public BeingCharacteristics Construct(int team, ulong step, float ci)
+		public BeingCharacteristics Construct(ulong step, float ci)
 		{
-			return new BeingCharacteristics(Guid.NewGuid(), 0, 100.0f, 5.0f, 1);
+			return new BeingCharacteristics(Guid.NewGuid(), 100.0f, 5.0f, 1);
 		}
 
 		public GameAction Think(ulong step, BeingCharacteristics characteristics, IWorldCell[,] area)
