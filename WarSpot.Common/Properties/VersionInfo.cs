80fdc6a827c74ffb264e70eb88557195f3f8b2b4
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-25 10:50:08 +0000

    small renames

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.1.271.80")]
-[assembly: AssemblyFileVersion("1.1.271.80")]
+[assembly: AssemblyVersion("1.1.275.84")]
+[assembly: AssemblyFileVersion("1.1.275.84")]
