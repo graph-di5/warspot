66f6efcd796c071556d723edbf3efe82c1acbfb9
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-25 16:55:52 +0000

    updated VersionInfo.cs for console computer

--- trunk/WarSpot.ConsoleComputer/Properties/AssemblyInfo.cs
+++ trunk/WarSpot.ConsoleComputer/Properties/AssemblyInfo.cs
@@ -8,9 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WarSpot.ConsoleComputer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("WarSpot.ConsoleComputer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +20,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9c38040f-66ea-4e62-80ca-8062ede7ff4a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
