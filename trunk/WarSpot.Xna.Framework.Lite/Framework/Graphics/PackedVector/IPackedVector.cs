cd9d057c0749c0dc057a4f16a1033aac31fc2b7e
pavel.v.fedotovsky@gmail.com <pavel.v.fedotovsky@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-11-25 15:33:58 +0000

    Removed XNA.Framework.Lite.

--- trunk/WarSpot.Xna.Framework.Lite/Framework/Graphics/PackedVector/IPackedVector.cs
+++
@@ -1,9 +0,0 @@
-namespace WarSpot.XNA.Framework.Graphics.PackedVector
-{
-	public interface IPackedVector
-    {
-        void PackFromVector4(Vector4 vector);
-        Vector4 ToVector4();
-    }
-}
-
