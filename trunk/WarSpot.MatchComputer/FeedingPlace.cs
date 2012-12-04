ee821e71bc09c9e26ceda12866b40ef56e0b8cf4
novgosh <novgosh@4011d52e-8f18-959e-644a-452fe91a9f5f>
2012-12-04 11:12:26 +0000

    Добавлен новый класс - место спавна пищи

---
+++ trunk/WarSpot.MatchComputer/FeedingPlace.cs
@@ -0,0 +1,48 @@
+﻿	using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarSpot.MatchComputer
+{
+	class FeedingPlace
+	{
+		public int X
+		{
+			get;
+			private set;
+		}
+		public int Y
+		{
+			get;
+			private set;
+		}
+
+		private float _maxCiCount;
+		private float _producingSpeed;
+		private World _world;
+		private List<WarSpotEvent> _eventsHistory;
+
+		public FeedingPlace (World world, List<WarSpotEvent> eventsHistory, int x, int y, float maxCiCount, float producingSpeed)
+		{
+			_world = world;
+			_eventsHistory = eventsHistory;
+			X = x;
+			Y = y;
+			_maxCiCount = maxCiCount;
+			_producingSpeed = producingSpeed;
+
+			world[X, Y].Ci = Math.Max(world[X, Y].Ci, maxCiCount); 
+			_eventsHistory.Add(new GameEventWorldCiChanged(X, Y, world[X, Y].Ci)); 
+		}
+		
+		public void ProduceNewCi ()
+		{
+			if (_world[X, Y].Ci < _maxCiCount)
+			{
+				_world[X, Y].Ci = Math.Min(_maxCiCount, _world[X, Y].Ci + _producingSpeed);
+				_eventsHistory.Add(new GameEventWorldCiChanged(X, Y, _world[X, Y].Ci)); 
+			}	
+		}
+	}
+}
