ccb1e95ed78c08a803889884e399095a05a1c011
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-10 22:14:45 +0000

    small updated in setup project

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.4.394.179")]
-[assembly: AssemblyFileVersion("1.4.394.179")]
+[assembly: AssemblyVersion("1.4.409.186")]
+[assembly: AssemblyFileVersion("1.4.409.186")]
