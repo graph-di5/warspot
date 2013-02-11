b457f94d52fc7bf86f832fa7500b7158dd877ba5
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-11 22:08:11 +0000

    another ccnet test

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.5.423.203")]
-[assembly: AssemblyFileVersion("1.5.423.203")]
+[assembly: AssemblyVersion("1.5.423.210")]
+[assembly: AssemblyFileVersion("1.5.423.210")]
