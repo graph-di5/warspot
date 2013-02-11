a4174297557f04d6096b4a133ceadfef0b8b0725
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-11 21:05:45 +0000

    temporary excluded WebFace from common solution for build consistency.

--- trunk/WarSpot.Common/Properties/VersionInfo.cs
+++ trunk/WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.5.418.200")]
-[assembly: AssemblyFileVersion("1.5.418.200")]
+[assembly: AssemblyVersion("1.5.422.203")]
+[assembly: AssemblyFileVersion("1.5.422.203")]
