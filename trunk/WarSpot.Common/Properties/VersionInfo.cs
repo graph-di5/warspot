d425c1505376b492ce157da90ccf6e8726cb98e6
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-13 18:35:19 +0000

    test ccnet

--- trunk/WarSpot.Common/Properties/VersionInfo.cs
+++ trunk/WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.5.423.210")]
-[assembly: AssemblyFileVersion("1.5.423.210")]
+[assembly: AssemblyVersion("1.5.422.223")]
+[assembly: AssemblyFileVersion("1.5.422.223")]
