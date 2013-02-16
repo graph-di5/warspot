d280667335f055caf0c69def8b70d137705ad6d3
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-16 22:12:23 +0000

    to r438

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.5.422.359")]
-[assembly: AssemblyFileVersion("1.5.422.359")]
+[assembly: AssemblyVersion("1.6.422.359")]
+[assembly: AssemblyFileVersion("1.6.422.359")]
