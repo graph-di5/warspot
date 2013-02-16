a585ebbe8d1dea1c88b73e87a07bb673d4a8469e
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-16 12:23:47 +0000

    1.Переписал действия "Передать энергию" и "Лечить" на обращение к цели по клетке. 2.Соответственно подправил Computer.cs. 3.Если при передаче энергии целевая клетка окажется пустой--энергия отправится в неё.

--- WarSpot.Contracts.Intellect/Actions/GameActionTreat.cs
+++ WarSpot.Contracts.Intellect/Actions/GameActionTreat.cs
@@ -4,16 +4,20 @@ namespace WarSpot.Contracts.Intellect.Actions
 {
 	public class GameActionTreat : GameAction
 	{
+        public int X { get; private set; }
+        public int Y { get; private set; }
 		public float UsingCi { private set; get; }
 
 		/// <summary>
-		/// Theat target using Ci energy.
+        /// Theat target being using Ci energy targeting to cell (relatively to actor).
 		/// </summary>
-		public GameActionTreat(Guid senderId, Guid targetId, float Ci) : base(senderId)
+		public GameActionTreat(Guid senderId, int x, int y, float ci) : base(senderId)
 		{
 			ActionType = ActionTypes.GameActionTreat;
-			UsingCi = Ci;
-			TargetId = targetId;
+            X = x;
+            Y = y;
+			UsingCi = ci;
+			
 		}
 
 		public Guid TargetId { get; private set; }
