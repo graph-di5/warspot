7cafd477ab35b11c32854d9f400be651936a6d9b
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-23 11:37:39 +0000

    fixed some role stuff

--- trunk/WarSpot.Cloud.Storage/Role.cs
+++ trunk/WarSpot.Cloud.Storage/Role.cs
@@ -11,42 +11,4 @@ namespace WarSpot.Cloud.Storage
 		Developer = 0x4,
 		NoUser = 0x1000000,
 	}
-	public static class Role
-	{
-
-		public static int GetRoleCode(string role)
-		{
-			switch (role)
-			{
-			case "MetaAdmin":
-				return (int)RoleType.MetaAdmin;
-			case "Admin":
-				return (int)RoleType.Admin;
-			case "User":
-				return (int)RoleType.User;
-			default:
-				return -1;
-			}
-		}
-
-		public static string GetRoleName(int id)
-		{
-			return ((RoleType) id).ToString();
-
-			switch ((RoleType)id)
-			{
-			case RoleType.User:
-				return RoleType.User.ToString();
-			case RoleType.Admin:
-				return RoleType.Admin.ToString();
-			case RoleType.MetaAdmin:
-				return RoleType.MetaAdmin.ToString();
-			case RoleType.Developer:
-				return RoleType.Developer.ToString();
-			default:
-				return RoleType.NoUser.ToString();
-
-			}
-		}
-	}
 }
