ff872b68f07ec343f5cc60c4ca9ff4291ab49e56
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-24 21:55:30 +0000

    fixed version helper

---
+++ WarSpot.Common/Utils/VersionHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace WarSpot.Common.Utils
+{
+	public class VersionHelper
+	{
+		public static Version Version
+		{
+			get { return Assembly.GetAssembly(typeof(VersionHelper)).GetName().Version; }
+		}
+
+#if fasle
+		public static bool CheckVersionFromStream(Stream fs, Version currVersion)
+		{
+			var bf = new BinaryFormatter();
+			var wsVersion = (Version)bf.Deserialize(fs);
+			return CheckVersion(currVersion, wsVersion);
+		}
+
+		public static bool CheckVersion(Version currVersion, Version versToCheck)
+		{
+			return (currVersion.Major == versToCheck.Major && currVersion.Minor == versToCheck.Minor);
+		}
+#endif
+
+		public static bool CheckVersionFromStream(Stream fs)
+		{
+			var bf = new BinaryFormatter();
+			var wsVersion = (Version)bf.Deserialize(fs);
+			return CheckVersion(wsVersion);
+		}
+
+
+		public static bool CheckVersion(Version versToCheck)
+		{
+			return (Version.Major == versToCheck.Major && Version.Minor == versToCheck.Minor);
+		}
+	}
+}
