71c6542ca215af666798b1cf35d1f2e7d2409ea9
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-02-25 22:02:15 +0000

    checked download of replayes

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.6.494.414")]
-[assembly: AssemblyFileVersion("1.6.494.414")]
+[assembly: AssemblyVersion("1.6.494.415")]
+[assembly: AssemblyFileVersion("1.6.494.415")]
