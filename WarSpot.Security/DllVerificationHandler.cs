a53a0c5c84eb3763d2e3a45f9e7845bc891e6541
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-01 12:59:17 +0000

    Added new errortypes to ErrorCode.cs and modified methods in contract that return ErrorCode for correct returning of ErrorType.
    Add new project WarSpot.Security.

---
+++ WarSpot.Security/DllVerificationHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarSpot.Security
+{
+    public static class DllVerificationHandler
+    {
+
+        public static void Execute(byte[] intellect)
+        {
+            // TO DO: EVERYTHING, DUDE.
+        }
+
+    }
+}
