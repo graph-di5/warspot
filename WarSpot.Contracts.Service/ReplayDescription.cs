cba87b2a3372c696ab3d032829298cfcc5438a34
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-01 12:10:06 +0000

    Strange but i didn't add ReplayDescription.cs in previous commit.
    Also "modified" ReplayDescription: added get\set and deleted constructor.

---
+++ WarSpot.Contracts.Service/ReplayDescription.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WarSpot.Contracts.Service
+{
+    [DataContract]
+    public class ReplayDescription
+    {
+        [DataMember]
+        public Guid id { get; set; }
+
+        [DataMember]
+        public string name { get; set; } 
+
+        [DataMember]
+        public List<string> intellects;
+
+        /*
+        public Guid ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<string> Intellects
+        {
+            get { return intellects; }
+            set { intellects = value; }
+        }
+
+        */
+ 
+        
+    }
+}
