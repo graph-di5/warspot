f983c89c3df503265bfc0b56c08e76424659e7b2
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-01 14:45:38 +0000

    fix added WarSpot.Security.csproj

--- trunk/WarSpot.Common/Properties/VersionInfo.cs
+++ trunk/WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.6.507.425")]
-[assembly: AssemblyFileVersion("1.6.507.425")]
+[assembly: AssemblyVersion("1.6.521.431")]
+[assembly: AssemblyFileVersion("1.6.521.431")]
