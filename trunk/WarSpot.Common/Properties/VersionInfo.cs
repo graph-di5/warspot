46cb8418013a7d7323a11bf70f2bbfeb52ec6089
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-02 13:39:23 +0000

    test commit

--- trunk/WarSpot.Common/Properties/VersionInfo.cs
+++ trunk/WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.6.529.439")]
-[assembly: AssemblyFileVersion("1.6.529.439")]
+[assembly: AssemblyVersion("1.6.530.439")]
+[assembly: AssemblyFileVersion("1.6.530.439")]
