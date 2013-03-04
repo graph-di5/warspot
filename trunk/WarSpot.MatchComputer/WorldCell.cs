7717c6d85d649cd96506efa78c68bb29b1d6a088
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-04 22:45:21 +0000

    updated info for world cells

--- trunk/WarSpot.MatchComputer/WorldCell.cs
+++ trunk/WarSpot.MatchComputer/WorldCell.cs
@@ -7,14 +7,34 @@ namespace WarSpot.MatchComputer
 	/// </summary>
 	class WorldCell
 	{
-		public int X { get; set; }
+		public int X {  get; private set; }
 
-		public int Y { get; set; }
+		public int Y { get; private set; }
 
 		public float Ci { get; set; }
 
 		// todo //!! delete this and merge
-		public Being BeingValue;
+		private Being _beingValue;
+		public Being BeingValue
+		{
+			get
+			{
+				return _beingValue;
+			}
+			set
+			{
+				_beingValue = value;
+				BeingValue.Characteristics.X = X;
+				BeingValue.Characteristics.Y = Y;
+			}
+		}
+
+		public WorldCell(int x, int y)
+		{
+			Y = y;
+			X = x;
+		}
+
 #if false
 		public IBeingInterface Being
 		{
