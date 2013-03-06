98de6fe7688837c954eae0bd20035e852db54947
stanislav.sartasov@gmail.com <stanislav.sartasov@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-06 17:48:47 +0000

    - first iteration over Metro client

---
+++ Warspot.MetroClient/ViewModel/ServiceLocator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarSpot.MetroClient.ViewModel
+{
+    public class ServiceLocator
+    {
+        static UnityContainer _container = new UnityContainer();
+
+        static ServiceLocator()
+        {
+            _container.RegisterType(typeof(ReplayViewModel),typeof(ReplayViewModel),"ReplayVM",new ContainerControlledLifetimeManager(),
+                new InjectionMember[0]);
+        }
+
+        public ServiceLocator()
+        {
+        }
+
+        public ReplayViewModel ReplayViewModel
+        {
+            get
+            {
+                return _container.Resolve<ReplayViewModel>("ReplayVM");
+            }
+        }
+    }
+}
