2fa18b750a7894509547e8a69131195b06ad2cdf
stanislav.sartasov@gmail.com <stanislav.sartasov@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-06 17:48:47 +0000

    - first iteration over Metro client

---
+++ trunk/Warspot.MetroClient/ServiceClient/ProxyServiceClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarSpot.MetroClient.ServiceClient
+{
+    class ProxyServiceClient : IWarSpotService
+    {
+    }
+}
---
+++ trunk/Warspot.MetroClient/ViewModel/ReplayViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WarSpot.MetroClient.ViewModel
+{
+    public class ReplayViewModel
+    {
+        const int MaximumSpeedConst = 16;
+
+        const int MinimumSpeedConst = 1;
+
+        public void LoadReplay(byte[] replay)
+        {
+            DataContractSerializer bf = new DataContractSerializer(typeof(Version));
+        }
+
+        public ICommand NextTurnCommand
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public ICommand PreviousTurnCommand
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public ICommand StopCommand
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public ICommand StartCommand
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public int MaximumSpeed
+        {
+            get
+            {
+                return MaximumSpeedConst;
+            }
+        }
+
+        public int MinimumSpeed
+        {
+            get
+            {
+                return MinimumSpeedConst;
+            }
+        }
+
+        public int CurrentSpeed
+        {
+            get;
+            set;
+        }
+    }
+}
