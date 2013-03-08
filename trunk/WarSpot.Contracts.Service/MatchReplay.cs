d72d6dd77a9fd039b11bff15b15dd24bfb2fef22
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-08 10:08:51 +0000

    Фикс

---
+++ trunk/WarSpot.Contracts.Service/MatchReplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml;
+using WarSpot.MatchComputer;
+
+namespace WarSpot.MatchComputer
+{
+    [DataContract]
+    public class MatchReplay//Используем этот класс для хранения и сериализации истории событий.
+    {
+        [DataMember]
+        Version AssemblyVersion { get; set; }
+
+        [CollectionDataContractAttribute]
+        public List<WarSpotEvent> Events;
+
+        public MatchReplay()
+        {
+            AssemblyVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        }
+    }
+}
