bfa5b86afe92687685db2675245504d893a13440
rpgerko@gmail.com <rpgerko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-08 15:25:16 +0000

    Начал делать вики страницы.

---
+++ trunk/WarSpot.WebFace/Controllers/DocumentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using System.IO;
+//using ProjectBase.Tools.Wiki;
+
+namespace WarSpot.WebFace.Controllers
+{
+    public class DocumentController : Controller
+    {
+        //
+        // GET: /Document/
+
+        public ActionResult Index()
+        {
+            //System.Reflection.Assembly a = System.Reflection.Assembly.GetEntryAssembly();
+            //string baseDir = System.IO.Path.GetDirectoryName(a.Location);
+            //StreamReader sr = new StreamReader(System.Environment.CurrentDirectory + "/wiki/EULA.wiki");
+
+            //return View(model: (new WikiConverter()).ConvertToHtml(sr.ReadToEnd()));
+
+            return View();
+        }
+
+    }
+}
