f78261c644f795479620289b1223629475e5185f
Stanislav.Sartasov@gmail.com <Stanislav.Sartasov@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-08 11:07:14 +0000

    - added  missing files

---
+++ trunk/WarSpot.Contracts.Service/SerializationHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using WarSpot.Common.Utils;
+
+namespace WarSpot.Contracts.Service
+{
+	public static class SerializationHelper
+	{
+		public static MatchReplay Deserialize(Stream fs)
+		{
+			var dcs = new DataContractSerializer(typeof(MatchReplay));
+		    var replay = dcs.ReadObject(fs) as MatchReplay;
+            if(!VersionHelper.CheckVersion(replay.AssemblyVersion))throw new InvalidDataException("Version incompatible");
+		    return replay;
+		}
+
+		/// <summary>
+		/// Returns deserializated list of GameEvents from replay
+		/// </summary>
+        public static MatchReplay Deserialize(string path)
+		{
+			using(var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+			    return Deserialize(fs);
+			}
+		}
+
+        public static byte[] Serialize(MatchReplay replay)
+        {
+            var ms = new MemoryStream();
+            var dcs = new DataContractSerializer(typeof(MatchReplay));
+            dcs.WriteObject(ms, replay);
+            return ms.ToArray();
+        }
+
+	}
+}
---
+++ trunk/WarSpot.Contracts.Service/WarSpotEvent.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+// todo spit to different files
+
+// ATTENTION: Constructors without parameters are for serializing deserializing
+
+namespace WarSpot.Contracts.Service
+{
+    [DataContract]
+	public enum EventTypes
+	{
+		#region Game events
+        [EnumMember]
+		GameEventHealthChange,
+        [EnumMember]
+		GameEventCiChange,
+        [EnumMember]
+		GameEventMove,
+        [EnumMember]
+		GameEventDeath,
+        [EnumMember]
+		GameEventBirth,
+        [EnumMember]
+		GameEventWorldCiChanged,
+		#endregion
+
+		#region System events
+        [EnumMember]
+		SystemEventWorldCreated,
+        [EnumMember]
+		SystemEventTurnStarted,
+        [EnumMember]
+		SystemEventCommandDead,
+        [EnumMember]
+		SystemEventCommandWin,
+        [EnumMember]
+		SystemEventMatchEnd
+		#endregion
+	};
+
+	[DataContract]
+	public abstract class WarSpotEvent
+	{
+		/// <summary>
+		/// Type of the event.
+		/// </summary>
+		[DataMember]
+		public EventTypes EventType { set; get; }
+	}
+
+	#region Game events
+	[DataContract]
+	public abstract class GameEvent : WarSpotEvent
+	{
+		protected GameEvent(Guid creator)
+		{
+			SubjectId = creator;
+		}
+
+        protected GameEvent()
+        {
+        }
+
+	    [DataMember]
+        public Guid SubjectId { get; set; }
+	}
+
+	[DataContract]
+	public class GameEventHealthChange : GameEvent
+	{
+
+		public GameEventHealthChange(Guid subjectId, float newHealth) :
+			base(subjectId)
+		{
+			EventType = EventTypes.GameEventHealthChange;
+			Health = newHealth;
+		}
+
+        public GameEventHealthChange()
+        {
+        }
+
+	    /// <summary>
+		/// new health
+		/// </summary>
+		[DataMember]
+		public float Health { get; set; }
+	}
+
+    [DataContract]
+    public class GameEventCiChange : GameEvent
+    {
+        public GameEventCiChange(Guid subjectId, float newCi) :
+            base(subjectId)
+        {
+            EventType = EventTypes.GameEventCiChange;
+            Ci = newCi;
+        }
+
+        public GameEventCiChange()
+        {
+        }
+
+        /// <summary>
+		/// new Ci
+		/// </summary>
+		[DataMember]
+		public float Ci { get; set; }
+	}
+
+	[DataContract]
+	public class GameEventMove : GameEvent
+	{
+		public GameEventMove(Guid subjectId, int shiftX, int shiftY) :
+			base(subjectId)
+		{
+			EventType = EventTypes.GameEventMove;
+			ShiftX = shiftX;
+			ShiftY = shiftY;
+		}
+
+        public GameEventMove()
+        {
+        }
+
+	    [DataMember]
+		public int ShiftX { get; private set; }
+
+        [DataMember]
+		public int ShiftY { get; private set; }
+	}
+
+    [DataContract]
+	public class GameEventDeath : GameEvent
+	{
+        public GameEventDeath()
+        {
+        }
+
+        public GameEventDeath(Guid creator)
+			: base(creator)
+		{
+			EventType = EventTypes.GameEventDeath;
+		}
+	}
+
+    [DataContract]
+	public class GameEventBirth : GameEvent
+	{
+        [DataMember]
+		public BeingCharacteristics Newborn { set; get; }
+
+        public GameEventBirth()
+        {
+        }
+
+        public GameEventBirth(Guid creator, BeingCharacteristics newborn)
+			: base(creator)
+		{
+			EventType = EventTypes.GameEventBirth;
+			Newborn = newborn;
+		}
+	}
+
+	[DataContract]
+	public class GameEventWorldCiChanged : WarSpotEvent
+	{
+		public GameEventWorldCiChanged(int x, int y, float ci)
+		{
+			EventType = EventTypes.GameEventWorldCiChanged;
+			X = x;
+			Y = y;
+			Ci = ci;
+		}
+
+        public GameEventWorldCiChanged()
+        {
+        }
+
+	    [DataMember]
+		public int X { get; set; }
+
+        [DataMember]
+		public int Y { get; set; }
+
+		/// <summary>
+		/// new Ci
+		/// </summary>
+        [DataMember]
+		public float Ci { get; set; }
+	}
+	#endregion
+
+	#region System events
+
+	[DataContract]
+	public abstract class SystemEvent : WarSpotEvent
+	{
+	}
+
+	[DataContract]
+	public class SystemEventWorldCreated : SystemEvent
+	{
+        [DataMember]
+		public int Width { set; get; }
+
+        [DataMember]
+		public int Height { set; get; }
+
+        public SystemEventWorldCreated()
+        {
+        }
+
+	    public SystemEventWorldCreated(int width, int height)
+		{
+			EventType = EventTypes.SystemEventWorldCreated;
+			Width = width;
+			Height = height;
+		}
+	}
+
+	[DataContract]
+	public class SystemEventTurnStarted : SystemEvent
+	{
+        [DataMember]
+		public ulong Number { set; get; }
+
+        public SystemEventTurnStarted()
+        {
+        }
+
+	    public SystemEventTurnStarted(ulong number)
+		{
+			EventType = EventTypes.SystemEventTurnStarted;
+			Number = number;
+		}
+	}
+
+    [DataContract]
+	public class SystemEventCommandDead : SystemEvent
+	{
+        public SystemEventCommandDead()
+        {
+        }
+
+		public SystemEventCommandDead(int teamId)
+		{
+			EventType = EventTypes.SystemEventCommandDead;
+			TeamId = teamId;
+		}
+
+        [DataMember]
+		public int TeamId { get; set; }
+	}
+
+    [DataContract]
+	public class SystemEventCommandWin : SystemEvent
+	{
+        public SystemEventCommandWin()
+        {
+        }
+
+        public SystemEventCommandWin(int teamId)
+		{
+			EventType = EventTypes.SystemEventCommandWin;
+			TeamId = teamId;
+		}
+
+        [DataMember]
+		public int TeamId { get; set; }
+	}
+
+    [DataContract]
+	public class SystemEventMatchEnd : SystemEvent
+	{
+		public SystemEventMatchEnd()
+		{
+			EventType = EventTypes.SystemEventMatchEnd;
+		}
+	}
+	#endregion
+}
