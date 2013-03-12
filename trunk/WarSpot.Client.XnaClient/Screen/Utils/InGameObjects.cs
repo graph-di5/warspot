f35f14aa3375831eab1251d3e450435f65be0708
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-12 21:08:12 +0000

    XNA client fixed

--- trunk/WarSpot.Client.XnaClient/Screen/Utils/InGameObjects.cs
+++ trunk/WarSpot.Client.XnaClient/Screen/Utils/InGameObjects.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace WarSpot.Client.XnaClient.Screen.Utils
 {
@@ -18,29 +14,51 @@ namespace WarSpot.Client.XnaClient.Screen.Utils
 	/// Class for storing data of all drawable objects
 	/// </summary>
 	class Creature
-	{		
+	{
+		private static int _teamsCount;
+		private static Dictionary<Guid, int> _teams;
+		public static void ResetTeams()
+		{
+			_teamsCount = 0;
+			_teams = new Dictionary<Guid, int>();
+		}
 		public Guid Id { get; private set; }
 		public int X { get; set; }
 		public int Y { get; set; }
-		public Guid Team { get; private set; }
+		public int Team { get; private set; }
 		public float MaxHealth { get; private set; }
 		public float CurrentHealth { get; set; }
 		public float CurrentCi { get; set; }
 		public float MaxCi { get; private set; }
 		public GameObjectType Type { get; private set; }
-		public bool isAlive;
+		public bool IsAlive;
 
-		public Creature(Guid id, int x, int y, Guid team, float maxHealth, float currHP, float currCi)
+		public Creature(Guid id, int x, int y, Guid team, float maxHealth, float currHp, float currCi)
 		{
+			if(_teams.ContainsKey(team))
+			{
+				Team = _teams[team];
+			}
+			else
+			{
+				if(team == Guid.Empty)
+				{
+					Team = 0;
+				}
+				else
+				{
+					Team = ++_teamsCount;
+					_teams.Add(team, _teamsCount);
+				}
+			}
 			Id = id;
 			X = x;
 			Y = y;
-			Team = team;
 			MaxHealth = maxHealth;
-			CurrentHealth = currHP;
+			CurrentHealth = currHp;
 			CurrentCi = currCi;
 			MaxCi = maxHealth;
-			isAlive = true;
+			IsAlive = true;
 		}
 	}
 
