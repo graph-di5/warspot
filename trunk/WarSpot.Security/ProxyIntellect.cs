d80d9f2cbe4c671407d90aa10b65688bc0b10a66
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-12 15:34:48 +0000

    [SECURITY]: Some ideas about using custom permission WarSpotSecurityPermission.

--- trunk/WarSpot.Security/ProxyIntellect.cs
+++ trunk/WarSpot.Security/ProxyIntellect.cs
@@ -17,21 +17,50 @@ namespace WarSpot.Security
         {
             this.intellect = null;
         }
-
-        
+       
         // Этим методом следует иницилизировать поле intellect?
-        public static bool Initialize(Assembly dll)
+        public bool Initialize(Assembly dll)
         {
+            this.intellect = AddBeing(dll).First<IBeingInterface>();
+
+            if (this.intellect == null)
+            {
+                return false;
+            }
             // TO DO: Initialize field intellect.
-            throw new NotImplementedException();
+            return true;
         }
 
+        private static List<IBeingInterface> AddBeing(Assembly assembly)
+        {
+            List<IBeingInterface> objects = new List<IBeingInterface>();
+            string iMyInterfaceName = typeof(IBeingInterface).ToString();
+            TypeDelegator[] defaultConstructorParametersTypes = new TypeDelegator[0];
+            object[] defaultConstructorParameters = new object[0];
+
+            IBeingInterface iAI = null;
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.GetInterface(iMyInterfaceName) != null)
+                {
+                    ConstructorInfo defaultConstructor = type.GetConstructor(defaultConstructorParametersTypes);
+                    object instance = defaultConstructor.Invoke(defaultConstructorParameters);
+                    iAI = (IBeingInterface)instance;//Достаём таки нужный интерфейс
+                    //
+                    objects.Add(iAI);
+                }
+            }
+            return objects;
+        }
+
+        [WarSpotSecurityPermission(SecurityAction.Demand)] // TO DO: 
         public BeingCharacteristics Construct(ulong step, float ci)
         {
             throw new NotImplementedException();
         }
 
-
+         // TO DO:
         public Contracts.Intellect.Actions.GameAction Think(ulong step, BeingCharacteristics characteristics, WorldInfo area)
         {
             throw new NotImplementedException();
