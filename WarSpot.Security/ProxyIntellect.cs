12299ab34d9269e06b35be0f5cdd00c95beb9b12
gregory.nitsenko@gmail.com <gregory.nitsenko@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-03-12 11:28:18 +0000

    [SECURITT]: Some ideas about SecurityProxy.

---
+++ WarSpot.Security/ProxyIntellect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarSpot.Contracts.Intellect;
+using System.Reflection;
+using System.Security.Permissions;
+
+namespace WarSpot.Security
+{
+    public class ProxyIntellect : IBeingInterface
+    {
+        // 
+        private IBeingInterface intellect;
+
+        public ProxyIntellect()
+        {
+            this.intellect = null;
+        }
+
+        
+        // Этим методом следует иницилизировать поле intellect?
+        public static bool Initialize(Assembly dll)
+        {
+            // TO DO: Initialize field intellect.
+            throw new NotImplementedException();
+        }
+
+        public BeingCharacteristics Construct(ulong step, float ci)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Contracts.Intellect.Actions.GameAction Think(ulong step, BeingCharacteristics characteristics, WorldInfo area)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
