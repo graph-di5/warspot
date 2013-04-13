76ffc9040ac18c769c5bc13c7cf22b05992e5c96
without.memory@gmail.com <without.memory@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-04-13 15:19:39 +0000

    Слил вместе два показательных интеллекта.

--- WarSpot.SampleIntellect/MainIntellectClass.cs
+++ WarSpot.SampleIntellect/MainIntellectClass.cs
@@ -1,6 +1,8 @@
 ﻿using WarSpot.Contracts.Intellect;
 using WarSpot.Contracts.Intellect.Actions;
 
+using System;//You can use outer libraries
+
 namespace WarSpot.SampleIntellect
 {
 	/// <summary>
@@ -8,7 +10,7 @@ namespace WarSpot.SampleIntellect
 	/// -------------------------------------------------------------------------
 	/// You can change the behavior of this intelligence, 
 	/// if this will not break the rules WarSpot.
-	/// You can use all algorithms in this class as you what.
+	/// You can use all algorithms in this class as you whant.
 	/// WarSpot team is not responsible for any damage caused by using the 
 	/// algorithms used in the class.
 	/// -------------------------------------------------------------------------
@@ -18,9 +20,100 @@ namespace WarSpot.SampleIntellect
 	/// where you need, and then use Debug->Run and step debugging.
 	/// 
 	/// </summary>
+
+	//You may create your own classes
+	public class Vector
+	{
+		public int X;
+		public int Y;
+
+		public Vector()
+		{
+			X = 0;
+			Y = 0;
+		}
+
+		public Vector(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+
 	public class MainIntellectClass : IBeingInterface
 	{
 		// Here you can place your own varables, constants or functions.
+		private float stepCost;
+
+		#region Action Methods
+		/// <summary>
+		/// Moves to the most appetizing cell within reach
+		/// </summary>
+		///  <param name="characteristics">Being characteristics</param>
+		///  <param name="area">Part of the world to looking-for</param>
+		/// <returns>Return preferable action </returns>
+		private GameAction goEat(BeingCharacteristics characteristics, WorldInfo area)
+		{
+			Vector _bestCell = new Vector();
+			float _bestCellScore = 0.0f;
+			int _maxStep = (int)(characteristics.Ci / stepCost);
+
+			if (_maxStep > characteristics.MaxStep)
+			{
+				_maxStep = (int)characteristics.MaxStep;
+			}
+
+			for (int i = -area.Distance; i < area.Distance; i++)
+			{
+				for (int j = -area.Distance; j < area.Distance; j++)
+				{
+					int _distance = Math.Abs(i) + Math.Abs(j);
+					float _profit = area[i, j].Ci - stepCost * (float)(_distance);
+
+					if (_profit > _bestCellScore && _distance <= _maxStep)
+					{
+						_bestCellScore = _profit;
+						_bestCell.X = i;
+						_bestCell.Y = j;
+					}
+				}
+			}
+
+			if (_bestCell.X == 0 && _bestCell.Y == 0)
+			{
+				return new GameActionEat(characteristics.Id);
+			}
+			else
+			{
+				return new GameActionMove(characteristics.Id, _bestCell.X, _bestCell.Y);
+			}
+		}
+		
+		/// <summary>
+		/// Heal itself
+		/// </summary>
+		///  <param name="characteristics">Being characteristics</param>
+		/// <returns>Return computed action</returns>
+		private GameAction selfHeal(BeingCharacteristics characteristics)
+		{
+			float _ci = 0.0f;
+			if (characteristics.Ci >= (characteristics.MaxHealth - characteristics.Health) * 3.0f) //Заметьте, он не проверяет, сколько энергии останется. А она нужна для того, чтобы жить.
+			{//Heal itself fully if it can
+				_ci = (characteristics.MaxHealth - characteristics.Health) * 3.0f;
+
+				if (_ci > characteristics.Ci * 0.6f)
+				{//If more than one permitted then reduces
+					_ci = characteristics.Ci * 0.6f;
+				}
+			}
+			else
+			{
+				_ci = Math.Abs(characteristics.Ci);
+			}
+
+			return new GameActionTreat(characteristics.Id, 0, 0, _ci);//In relative coordinates
+		}
+		#endregion
 
 		/// <summary>
 		/// This method is called on every being creation. Your intellect should distribute Ci to being abilities.
@@ -30,8 +123,8 @@ namespace WarSpot.SampleIntellect
 		/// <returns>Desired characteristic of the future being.</returns>
 		public BeingCharacteristics Construct(ulong step, float ci)
 		{
-			// small simple being with low characteristics
-			return new BeingCharacteristics(100, 1, 1);
+			//Being characteristics may be different. It's you to decide. But remember about the cost.
+			return new BeingCharacteristics((ci - 13.0f) / 0.8f, 3.0f, 4);
 		}
 
 		/// <summary>
@@ -42,16 +135,20 @@ namespace WarSpot.SampleIntellect
 		/// <param name="area">Information about world in the visiable area.</param>
 		/// <returns>GameAction that represents decision.</returns>
 		public GameAction Think(ulong step, BeingCharacteristics characteristics, WorldInfo area)
-		{
-			// if there's some Ci in current world cell we can eat it 
-			if(area[0, 0].Ci > 0)
+		{			
+			// Here you can place a lot of decesions 
+			if (characteristics.Health < characteristics.MaxHealth && characteristics.Ci > characteristics.MaxHealth * 0.6f)
 			{
-				return new GameActionEat(characteristics.Id);
+				return selfHeal(characteristics);
 			}
-			// here you can place a lot of decesions 
-
-			// default action
-			return new GameActionMove(characteristics.Id, 1, 0);
+			else if (characteristics.Ci > 13.0f + characteristics.MaxHealth * 0.8f)
+			{
+				return new GameActionMakeOffspring(characteristics.Id, 13.0f + characteristics.MaxHealth * 0.8f);
+			}
+			else
+			{
+				return goEat(characteristics, area);
+			}
 		}
 	}
 }
