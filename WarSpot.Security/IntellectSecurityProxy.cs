1a6b0fb0d4545ff3e30c5e0af720c56ac24bd67b
Stanislav.Sartasov@gmail.com <Stanislav.Sartasov@gmail.com@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-04-18 14:21:05 +0000

    - security upd

---
+++ WarSpot.Security/IntellectSecurityProxy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using WarSpot.Contracts.Intellect;
+using System.Reflection;
+using System.Net;
+using System.Security.Permissions;
+
+namespace WarSpot.Security
+{
+    public class IntellectSecurityProxy : MarshalByRefObject, IBeingInterface
+    {
+        // 
+        private IBeingInterface _intellect;
+
+        public IntellectSecurityProxy()
+        {
+            byte[] assembly = (byte[])AppDomain.CurrentDomain.GetData("Intellect");
+
+            var loadedAssembly = Assembly.Load(assembly);
+
+            var types = loadedAssembly.GetTypes();
+            var type = types.First(x => x.GetInterface("IBeingInterface") != null);
+            _intellect = (IBeingInterface) loadedAssembly.CreateInstance(type.FullName);
+        }
+
+
+        [FileIOPermissionAttribute(SecurityAction.Deny),
+            ReflectionPermissionAttribute(SecurityAction.Deny),
+                     SecurityPermissionAttribute(SecurityAction.PermitOnly, Execution = true),
+                            SocketPermission(SecurityAction.Deny)]
+        public BeingCharacteristics Construct(ulong step, float ci)
+        {
+            return _intellect.Construct(step, ci); //Таким образом, вызываем метод Construct у проверяемой библиотеки, 
+                                                       //с уже навешенной проверкой безопасности.
+        }
+
+        [FileIOPermissionAttribute(SecurityAction.Deny),
+            ReflectionPermissionAttribute(SecurityAction.Deny),
+                     SecurityPermissionAttribute(SecurityAction.PermitOnly, Execution = true),
+                            SocketPermissionAttribute(SecurityAction.Deny)]
+        public Contracts.Intellect.Actions.GameAction Think(ulong step, BeingCharacteristics characteristics, WorldInfo area)
+        {
+            return _intellect.Think(step, characteristics, area);
+        }
+    }
+}
