4d08a2315ef481f37fe3143204cae413b85aa754
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-04-20 10:20:04 +0000

    fix setup: added app.config for XNA

--- trunk/afterbuild.bat
+++ trunk/afterbuild.bat
@@ -7,10 +7,10 @@ xcopy /s /y bin\Content redistr\Content\
 copy /y bin\nuclex.*.dll redistr\
 copy /y bin\nunit.framework.dll redistr\
 copy /y bin\SlimDX.DirectInput.dll redistr\
-copy /y bin\WarSpot.Client.XnaClient.exe redistr\
+copy /y bin\WarSpot.Client.XnaClient.exe* redistr\
 copy /y bin\WarSpot.Common.dll redistr\
 copy /y bin\WarSpot.MatchComputer.dll redistr\
-copy /y bin\WarSpot.ConsoleComputer.exe redistr\
+copy /y bin\WarSpot.ConsoleComputer.exe* redistr\
 copy /y bin\WarSpot.Contracts.Intellect.dll redistr\
 copy /y bin\WarSpot.Contracts.Service.dll redistr\
 
