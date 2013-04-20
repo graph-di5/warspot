670e29340a21954f8e94a453754ce9a143d8933e
graph.di5 <graph.di5@4011d52e-8f18-959e-644a-452fe91a9f5f>
2013-04-20 11:59:31 +0000

    fixed tournament db. force reload added Details

--- WarSpot.Common/Properties/VersionInfo.cs
+++ WarSpot.Common/Properties/VersionInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 // 
 // Warning: last to fileds are autoupdatable. DO NOT CHANGE them manualy
 
-[assembly: AssemblyVersion("1.7.726.574")]
-[assembly: AssemblyFileVersion("1.7.726.574")]
+[assembly: AssemblyVersion("1.7.734.582")]
+[assembly: AssemblyFileVersion("1.7.734.582")]
